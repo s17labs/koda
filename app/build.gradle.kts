3d1107322f9a4cab758656d31c48846ab51a097d
Sam <64147848+yungsamd17@users.noreply.github.com>
2026-09-08 02:39:07 +0000

    release: v0.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.s17labs.koda"
         minSdk = 26
         targetSdk = 33
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
